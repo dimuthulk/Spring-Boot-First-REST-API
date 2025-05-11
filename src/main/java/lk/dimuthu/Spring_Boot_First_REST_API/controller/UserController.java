86148acf3d376e1c49f0c6d254b2025a447df6bc
Dimuthu Lakmal <info.dlrathnayaka@gmail.com>
2025-05-11 18:52:40 +0530

    app methods

--- src/main/java/lk/dimuthu/Spring_Boot_First_REST_API/controller/UserController.java
+++ src/main/java/lk/dimuthu/Spring_Boot_First_REST_API/controller/UserController.java
@@ -1,4 +1,17 @@
 package lk.dimuthu.Spring_Boot_First_REST_API.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/user")
+@CrossOrigin
+@RestController
 public class UserController {
+
+    @GetMapping("/get")
+    public String testAPI(){
+        return "API Success!";
+    }
 }
