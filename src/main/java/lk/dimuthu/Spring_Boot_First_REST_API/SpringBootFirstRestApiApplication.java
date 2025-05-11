e23a557e0bf03dfad33cdfa1c7d5d6c8b2d163b8
Dimuthu Lakmal <info.dlrathnayaka@gmail.com>
2025-05-11 18:23:56 +0530

    initialize commit

---
+++ src/main/java/lk/dimuthu/Spring_Boot_First_REST_API/SpringBootFirstRestApiApplication.java
@@ -0,0 +1,13 @@
+package lk.dimuthu.Spring_Boot_First_REST_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/dimuthu/Spring_Boot_First_REST_API/SpringBootFirstRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dimuthu.Spring_Boot_First_REST_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
