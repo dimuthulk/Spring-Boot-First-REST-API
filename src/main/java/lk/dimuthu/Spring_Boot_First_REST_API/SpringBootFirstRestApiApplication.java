75245d97f2a1a89a86639ad98197a699bc20cfe3
Dimuthu Lakmal <info.dlrathnayaka@gmail.com>
2025-05-13 17:38:35 +0530

    update

--- src/main/java/lk/dimuthu/Spring_Boot_First_REST_API/SpringBootFirstRestApiApplication.java
+++ src/main/java/lk/dimuthu/Spring_Boot_First_REST_API/SpringBootFirstRestApiApplication.java
@@ -2,12 +2,20 @@ package lk.dimuthu.Spring_Boot_First_REST_API;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController()
 public class SpringBootFirstRestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootFirstRestApiApplication.class, args);
+		System.out.println("Server is running http://localhost:8080/");
+	}
+
+	@GetMapping(value = "/")
+	public String test(){
+		return "API Success!";
 	}
 
 }
